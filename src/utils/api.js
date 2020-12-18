c7aed611c92934e3888f64f9b96c8d9d43fbb1a3
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2020-12-18 08:35:04 -0500

    Refactored to use env variables

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,4 @@
-export const YOUTUBE_API_KEY = '';
-export const TMDB_API_KEY = '';
+const envVariables = process.env;
+
+export const YOUTUBE_API_KEY = envVariables.REACT_APP_YOUTUBE_API_KEY;
+export const TMDB_API_KEY = envVariables.REACT_APP_TMDB_API_KEY;
